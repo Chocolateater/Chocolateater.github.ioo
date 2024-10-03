91703a782b29d50493307f5d00d1414356a1c576
Chocolateater <boxxyt@163.com>
2024-10-03 16:56:31 +0800

    Site updated: 2024-10-03 16:56:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/03/hello-world/"];function toRandomPost(){
+var posts=["2024/10/03/Github图床折腾手记/","2024/10/03/完美匹配/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
